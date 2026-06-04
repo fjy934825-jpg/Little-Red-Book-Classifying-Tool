580f8b191b08b5033420e72f17ec8d5bb69e73d5
fjy934825 <fjy934825@gmail.com>
2026-06-04 17:07:40 +0800

    Add editing for organized entries

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "xhs-content-organizer-v9";
+const CACHE_NAME = "xhs-content-organizer-v10";
 const ASSETS = ["./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
