f11d05ff64816ff375dc1335d3f0a7e55dac8268
fjy934825 <fjy934825@gmail.com>
2026-06-05 18:14:16 +0800

    Refresh visual design

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "xhs-content-organizer-v13";
+const CACHE_NAME = "xhs-content-organizer-v14";
 const ASSETS = ["./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
